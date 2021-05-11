4652272819444af5c3a0e0104da8b8871443be47
Sventuh <sventuhcorp@gmail.com>
2021-05-11 22:02:04 +0300

    who is on duty today

---
+++ Firsteautotest.spec.js
@@ -0,0 +1,11 @@
+///<reference types="cypress" />
+
+it ('User should be able to login with valid data', () => {
+cy.visit('https://react-redux.realworld.io');
+cy.get(':nth-child(2) > .nav-link').click();
+cy.get(':nth-child(1) > .form-control').type('sventuhcorp@gmail.com').should('have.value', 'sventuhcorp@gmail.com');
+cy.get(':nth-child(2) > .form-control').type('drogba10').should('have.value', 'drogba10');
+cy.get('.btn').click();
+cy.get(':nth-child(4) > .nav-link').contains('Sventuh').should('exist');
+});
+          

655ee65caf9252fbd29775dfe8e2dda51d65fffc
Sventuh <sventuhcorp@gmail.com>
2021-05-12 16:44:04 +0300

    Bohdan Home Work

--- HomeworkSventuh.spec.js
+++ HomeworkSventuh.spec.js
@@ -1,15 +1,17 @@
 /// <reference types="cypress"/>
 
-it ('The second test', () => {
+it ('First test', () => {
   cy.visit('https://the-internet.herokuapp.com/login');
   cy.get('[name="username"]').type('tomsmith').should('have.value', 'tomsmith');
   cy.get('[name="password"]').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
   cy.get('button').click();
   cy.get('.icon-signout').should('contain', 'Logout');
- 
+});
+it ('Next test', () => {
   cy.get('.icon-signout').click();
   cy.get('[type="submit"]').should('contain', 'Login');
-
+});
+it ('Last test', () => {
   cy.get('[name="username"]').type('InvalidUser').should('have.value', 'InvalidUser');
   cy.get('[name="password"]').type('Awesomepassword').should('have.value', 'Awesomepassword');
   cy.get('button').click();
